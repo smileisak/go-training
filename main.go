25785fc62b596a28dd542a23cba53494f6621067
smileisak <ikaboubi@gmail.com>
2018-06-25 11:05:12 +0200

    variables and constants

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("[*] Variables")
+	fmt.Println("----------------")
+	Varibales()
+	fmt.Println("----------------")
+
+	fmt.Println("[*] Constants")
+	fmt.Println("----------------")
+	Constant()
+	fmt.Println("----------------")
+
+}
---
+++ variables.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// Variables is a declaration example
+func Varibales() {
+	// Declatre 1 or more variables
+	var a = "initial"
+	fmt.Println(a)
+
+	// Declare multiple variables at once
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	// Go will infer the type of initialized variables
+	var d = true
+	fmt.Println(d)
+
+	// Variables declared withiout a corresponding initialization are zero-valued. the zero value of init is 0
+	var e int
+	fmt.Println(e)
+
+	// The syntax is shorthand for declaring and initializing a variable
+	// In this case var f string = "short"
+	f := "short"
+	fmt.Println(f)
+}
