fbc4d249d4950aafdb0dce33da560d73b36fe862
smileisak <ikaboubi@gmail.com>
2018-07-09 11:26:02 +0200

    writing to files

--- reading-files.go
+++ reading-files.go
@@ -13,7 +13,7 @@ Reading and writing files are basic tasks needed for many Go programs.
 First we’ll look at some examples of reading files.
 */
 
-func check(e error) {
+func checkRead(e error) {
 	if e != nil {
 		panic(e)
 	}
@@ -25,25 +25,25 @@ func ReadFiles() {
 	// Perhaps the most basic file reading task is
 	// slurping a file’s entire contents into memory.
 	dat, err := ioutil.ReadFile("/tmp/dat")
-	check(err)
+	checkRead(err)
 	fmt.Print(string(dat))
 
 	// You’ll often want more control over how and what parts of a file are read.
 	// For these tasks, start by Opening a file to obtain an os.File value.
 	f, err := os.Open("/tmp/dat")
 	defer f.Close()
-	check(err)
+	checkRead(err)
 
 	// Read some bytes from the beginning of the file.
 	// Allow up to 5 to be read but also note how many actually were read.
 	b1 := make([]byte, 5)
 	n1, err := f.Read(b1)
-	check(err)
+	checkRead(err)
 	fmt.Printf("%d bytes: %s\n", n1, string(b1))
 
 	// You can also Seek to a known location in the file and Read from there.
 	o2, err := f.Seek(6, 0)
-	check(err)
+	checkRead(err)
 	b2 := make([]byte, 2)
 	n2, err := f.Read(b2)
 	fmt.Printf("%d bytes @ %d: %s\n", n2, o2, string(b2))
@@ -52,22 +52,22 @@ func ReadFiles() {
 	// For example,
 	// reads like the ones above can be more robustly implemented with ReadAtLeast.
 	o3, err := f.Seek(6, 0)
-	check(err)
+	checkRead(err)
 	b3 := make([]byte, 2)
 	n3, err := io.ReadAtLeast(f, b3, 2)
-	check(err)
+	checkRead(err)
 	fmt.Printf("%d bytes @ %d: %s\n", n3, o3, string(b3))
 
 	// There is no built-in rewind, but Seek(0, 0) accomplishes this.
 	// sets the file position to the beginning of the file of the given stream.
 	_, err = f.Seek(0, 0)
-	check(err)
+	checkRead(err)
 
 	// The bufio package implements a buffered reader that may be useful
 	// both for its efficiency with many small reads and because of the
 	// additional reading methods it provides.
 	r4 := bufio.NewReader(f)
 	b4, err := r4.Peek(5)
-	check(err)
+	checkRead(err)
 	fmt.Printf("5 bytes: %s\n", string(b4))
 }
